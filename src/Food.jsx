e3d1cf6f64c11575b8c7369dc7a6f69c48a7a6e5
JunKumagai-JP <kumagai@stch.st-ja.or.jp>
2023-07-20 16:26:29 +0900

    コミット２

---
+++ src/Food.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Food extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h1>テトブログの収益でステーキを食べたい2！</h1>
+            </div>
+        )
+    }
+}
+
+export default Food;
